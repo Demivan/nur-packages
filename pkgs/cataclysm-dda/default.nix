6606dc9ed8c1070c81b2b4c93cc6507e8176bcfe
Ivan Demchuk <ivan.demchuk@gmail.com>
2024-09-17 10:21:00 +0300

    Add cataclysm-dda

--- pkgs/cataclysm-dda/default.nix
+++ pkgs/cataclysm-dda/default.nix
@@ -3,13 +3,13 @@
   fetchFromGitHub,
 }:
 cataclysm-dda-git.overrideAttrs (old: {
-  version = "2024.06.02";
+  version = "2024-09-07-0110-experimental";
 
   src = fetchFromGitHub {
     owner = "CleverRaven";
     repo = "Cataclysm-DDA";
-    rev = "f608d5b853e0d9d8492e2bc64166d60cc5d21007";
-    sha256 = "sha256-h1hfIEpz8tETTvNAm2fD3RWaVoem55yce34NW3ATuqg=";
+    rev = "cdda-experimental-2024-09-07-0110";
+    sha256 = "sha256-eenBpUkR+boB9+q7YBCekeOADCWCwlKnxnnVWaaKX9g=";
   };
 
   patches = [
